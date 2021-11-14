a8facc8c415d84ac8ba58e826bcf770bfb41f6b5
ma7m0udalaa <ma7m0udalaa@icloud.com>
2021-11-14 15:39:01 +0200

    Router enhancement
    - Router direction with route function

--- VIPER.xctemplate/___FILEBASENAME___/Router/___FILEBASENAME___Router.swift
+++ VIPER.xctemplate/___FILEBASENAME___/Router/___FILEBASENAME___Router.swift
@@ -2,11 +2,15 @@
 
 import UIKit
 
-protocol ___FILEBASENAMEASIDENTIFIER___Protocol: BaseRouterProtocol {
+enum ___FILEBASENAMEASIDENTIFIER___Direction {
     
 }
 
-class ___FILEBASENAMEASIDENTIFIER___:  BaseRouter, ___FILEBASENAMEASIDENTIFIER___Protocol {
+protocol ___FILEBASENAMEASIDENTIFIER___Protocol: BaseRouterProtocol {
+    func route(_ to: ___FILEBASENAMEASIDENTIFIER___Direction)
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: BaseRouter, ___FILEBASENAMEASIDENTIFIER___Protocol {
     
     func createModule() -> UIViewController {
         let interactor = ___VARIABLE_productName___Interactor()
@@ -14,6 +18,10 @@ class ___FILEBASENAMEASIDENTIFIER___:  BaseRouter, ___FILEBASENAMEASIDENTIFIER__
         let presenter = ___VARIABLE_productName___Presenter(view: view, interactor: interactor, router: self)
         view.presenter = presenter
         return view
+    }
+    
+    func route(_ to: ___FILEBASENAMEASIDENTIFIER___Direction) {
+        
     }
     
 }
